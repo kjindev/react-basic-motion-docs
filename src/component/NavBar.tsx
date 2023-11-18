0447264caa57e542ba2879d548fb0ba86394ed2e
kjin <pkjin.eng@gmail.com>
2023-11-18 19:38:21 +0900

    feat: introduction page content

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -1,25 +1,19 @@
-import React, { useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 export default function NavBar() {
-  const { pathname } = useLocation();
-  useEffect(() => {
-    console.log(pathname);
-  }, [pathname]);
-  const titleStyle =
-    "m-2 p-2 font-bold hover:shadow rounded-lg hover:bg-slate-200 hover:cursor-pointer text-sm";
-  const listStyle =
-    "mx-3 my-1 px-3 py-2 hover:shadow rounded-lg hover:bg-zinc-300 hover:cursor-pointer text-sm hover:text-black";
-
+  // const { pathname } = useLocation();
   const animationList = ["Bounce", "Fade", "Pulse"];
   const loadingList = ["Ball", "Skeleton"];
 
   return (
     <div className="fixed bg-zinc-800 text-zinc-100 p-3 w-[270px] h-[100vh] shadow-sm flex flex-col justify-between">
       <div>
-        <div className="text-xl text-center px-3 py-2 mt-5">
-          React Basic Motion
-        </div>
+        <Link to="/">
+          <div className="text-xl text-center px-3 py-2 mt-5">
+            React Basic Motion
+          </div>
+        </Link>
+
         {/* <div className="flex flex-col">
         <Link to="/" className={titleStyle}>
           Introduction
@@ -57,7 +51,7 @@ export default function NavBar() {
         </div>
       </div>
       <div className="text-center text-sm py-2 text-zinc-300">
-        version 2.0.1
+        version 2.0.2
       </div>
     </div>
   );
--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -1,3 +1,24 @@
+import { Bounce, Pulse } from "react-basic-motion";
+
 export default function Introduction() {
-  return <div>Introduction</div>;
+  return (
+    <div className="pl-[330px] w-[100%] h-[100vh] p-10 flex flex-col items-center justify-center">
+      <div className="text-3xl font-bold flex">
+        <p>Hi, It's react-basic-motion!</p>
+        <Bounce
+          name="x"
+          range={10}
+          duration={1.5}
+          timing="ease-in-out"
+          count="infinite"
+        >
+          😊
+        </Bounce>
+      </div>
+      <div className="my-7 text-lg">
+        <p>Add animation on your project.</p>
+        <p>Make it easy to put loading ui on your page.</p>
+      </div>
+    </div>
+  );
 }
