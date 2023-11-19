acb7b85459cc2ba736a740a754fb7288f889a26c
kjin <pkjin.eng@gmail.com>
2023-11-19 18:20:37 +0900

    style: introduction mobile content

--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -2,7 +2,7 @@ import { Bounce, Pulse } from "react-basic-motion";
 
 export default function Introduction() {
   return (
-    <div className="pl-[330px] w-[100%] h-[100vh] p-10 flex flex-col items-center justify-center">
+    <div className="tab:pl-[330px] w-[100%] h-[100vh] p-10 flex flex-col items-center justify-center">
       <div className="text-3xl font-bold flex">
         <p>Hi, It's react-basic-motion!</p>
         <Bounce
